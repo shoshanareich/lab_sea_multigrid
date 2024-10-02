8499bee62fddca4b3300fd4b56567b196ff8ee60
Shoshi Reich <shoshi@sverdrup.oden.utexas.edu>
2024-10-02 11:59:56 -0500

    fix low-to-high interpolation

--- interp_xx_lores_to_hires_itX.py
+++ interp_xx_lores_to_hires_itX.py
@@ -1,6 +1,5 @@
 import numpy as np
-from scipy.interpolate import griddata, LinearNDInterpolator, NearestNDInterpolator
-from scipy.ndimage import generic_filter
+from scipy.interpolate import griddata, Rbf 
 import sys
 sys.path.append('/home/shoshi/MITgcm_c68r/MITgcm/utils/python/MITgcmutils')
 from MITgcmutils import rdmds
@@ -14,17 +13,17 @@ factor = 4 # lowres * factor = hires
 nxh=nx*factor
 nyh=ny*factor
 
-dirroot='/home/shoshi/MITgcm_c68r/MITgcm/verification/lab_sea/'
+#dirroot='/home/shoshi/MITgcm_c68r/MITgcm/verification/lab_sea/'
+dirroot='/scratch/shoshi/labsea_MG/bathy_tests/'
 
 #iter = '0000'
 iter = sys.argv[1] # sys.argv[0] is name of python file
-#interp = sys.argv[2]
 print(iter)
-#print(interp)
-interp = 'linear'
 
-dirrun_lr = dirroot + 'grid_lores/'
-dirrun_hr = dirroot + 'grid_hires_linear/'
+#dirrun_lr = dirroot + 'grid_lores/'
+#dirrun_hr = dirroot + 'grid_hires_linear/'
+dirrun_lr = dirroot + 'run_adlo_2lev_seaice_it0000_bad_bathy/'
+dirrun_hr = dirroot + 'run_adhi_2lev_seaice_it0000_bad_bathy/'
 
 #dirrun_pup = dirroot + 'run_adlo_noseaice_costsst_' + interp + '_packunpack_tape24hr/'
 dirrun_pup = '/scratch/shoshi/labsea_MG/bathy_tests/run_adlo_2lev_seaice_packunpack_bad_bathy/'
@@ -45,110 +44,63 @@ yc_hr = rdmds(dirrun_hr + 'YC')
 # hfacc_hr = rdmds(dirrun_hr + 'hFacC')
 hfacc_hr = rdmds('/scratch/shoshi/labsea_MG/bathy_tests/run_adhi_2lev_seaice_it0000_new_bathy/hFacC')
 
-# now we have the choice between interpolating lores xx to hires xx using
-# linear or nearest.  An used 'linear' for now
+# use linear interpolation, then have the choice between rbf and nearest to fix edges
+# use rbf for now
 
-# Replace NaNs with the mean of their neighbors
-def inpaint_nans(arr):
-    isnan = np.isnan(arr)
-    arr[isnan] = generic_filter(arr, np.nanmean, size=3)[isnan] # average of 3x3 square of points
-    return arr
+def linear_interp(xx_lr, dim):
 
-# fix weird border anns
-def fill_border_nans(arr):
-    # Fill NaNs in rows
-    for i in range(arr.shape[0]):
-        row_non_nans = np.where(~np.isnan(arr[i, :]))[0]
-        if len(row_non_nans) > 0:
-            if np.isnan(arr[i, 0]):
-                arr[i, 0] = arr[i, row_non_nans[0]]
-            if np.isnan(arr[i, -1]):
-                arr[i, -1] = arr[i, row_non_nans[-1]]
+    points = np.array([xc_lr.ravel(), yc_lr.ravel()]).T
     
-    # Fill NaNs in columns
-    for j in range(arr.shape[1]):
-        col_non_nans = np.where(~np.isnan(arr[:, j]))[0]
-        if len(col_non_nans) > 0:
-            if np.isnan(arr[0, j]):
-                arr[0, j] = arr[col_non_nans[0], j]
-            if np.isnan(arr[-1, j]):
-                arr[-1, j] = arr[col_non_nans[-1], j]
-
-    return arr
-
-def linear_interp(xx_lr):
-
     xx_hr = np.zeros((xx_lr.shape[0], nyh, nxh))
     for i in range(xx_lr.shape[0]):
         
-        # linear interpolation
-        tmp = griddata((xc_lr.ravel(), yc_lr.ravel()), xx_lr[i,:,:].ravel(), (xc_hr.ravel(), yc_hr.ravel()), method='linear')
-        tmp = tmp.reshape(xc_hr.shape[0], xc_hr.shape[1])
+        values = xx_lr[i,:,:].ravel()
+    
+        # Create a mask to filter out NaN values in the low-resolution data
+        mask = ~np.isnan(values)
+    
+        # Interpolate onto the high-resolution grid using linear interpolation
+        tmp = griddata(points[mask], values[mask], (xc_hr, yc_hr), method='linear')
+    
+        # Identify where the high-resolution grid still has NaN values after linear interpolation
+        nan_mask = np.isnan(tmp)
+    
+        # Apply RBF interpolation to fill the remaining NaN values
+        if np.any(nan_mask):
+            xc_hr_flat = xc_hr[nan_mask]
+            yc_hr_flat = yc_hr[nan_mask]
         
-        # handle NaNs
-        tmp = inpaint_nans(tmp)
-        
+            rbf = Rbf(points[mask][:, 0], points[mask][:, 1], values[mask], function='linear')
+            tmp[nan_mask] = rbf(xc_hr_flat, yc_hr_flat)
+
+        # or apply nn interpolation to fill the remaining NaN values
+        # tmp[nan_mask] = griddata(points[mask], values[mask], (xc_hr[nan_mask], yc_hr[nan_mask]), method='nearest')
+    
         # set floor and ceiling to that of low-res  
         lmin = np.min(xx_lr[i,:,:])
         lmax = np.max(xx_lr[i,:,:])
         tmp = np.clip(tmp, lmin, lmax)
 
-        tmp = fill_border_nans(tmp)
-        
-        xx_hr[i,:,:] = tmp
+        if dim == 2:
+            xx_hr[i,:,:] = tmp * hfacc_hr[0,:,:]
+        elif dim == 3:
+            xx_hr[i,:,:] = tmp * hfacc_hr[i,:,:]
 
     return xx_hr
 
 
-# try nearest neighbor interpolation
-def nearest_interp(xx_lr):
-
-    xx_hr = np.zeros((xx_lr.shape[0], nyh, nxh))
-    for i in range(xx_lr.shape[0]):
-        
-        # linear interpolation
-        tmp = griddata((xc_lr.ravel(), yc_lr.ravel()), xx_lr[i,:,:].ravel(), (xc_hr.ravel(), yc_hr.ravel()), method='nearest')
-        tmp = tmp.reshape(xc_hr.shape[0], xc_hr.shape[1])
-        
-        # handle NaNs
-        tmp = inpaint_nans(tmp)
-        
-        # set floor and ceiling to that of low-res  
-        lmin = np.min(xx_lr[i,:,:])
-        lmax = np.max(xx_lr[i,:,:])
-        tmp = np.clip(tmp, lmin, lmax)
-        
-        xx_hr[i,:,:] = tmp
-
-    return xx_hr
 
 def write_float64(fout,fld):
     with open(fout, 'wb') as f:
         np.array(fld, dtype=">f8").tofile(f)
 
 
+xx_theta_hr = linear_interp(xx_theta, 3)
+xx_atemp_hr = linear_interp(xx_atemp, 2)
 
-if interp == 'linear':
-    
-    # linear interpolation
-    xx_theta_hr = linear_interp(xx_theta)*hfacc_hr
-    xx_atemp_hr = linear_interp(xx_atemp)*hfacc_hr[0,:,:]
+xx_theta_hr[np.isnan(xx_theta_hr)] = 0
+xx_atemp_hr[np.isnan(xx_atemp_hr)] = 0
 
-    xx_theta_hr[np.isnan(xx_theta_hr)] = 0
-    xx_atemp_hr[np.isnan(xx_atemp_hr)] = 0
-
-    write_float64(dirrun_pup + 'xx_theta_hr.000000' + iter + '.data', xx_theta_hr)
-    write_float64(dirrun_pup + 'xx_atemp_hr.000000' + iter + '.data', xx_atemp_hr)
-
-elif interp == 'nearest':
-    
-    # nn interpolation
-    xx_theta_hr = nearest_interp(xx_theta)*hfacc_hr
-    xx_atemp_hr = nearest_interp(xx_atemp)*hfacc_hr[0,:,:]
-
-    xx_theta_hr[np.isnan(xx_theta_hr)] = 0
-    xx_atemp_hr[np.isnan(xx_atemp_hr)] = 0
-
-    write_float64(dirrun_pup + 'xx_theta_nearest.000000' + iter + '.data', xx_theta_hr)
-    write_float64(dirrun_pup + 'xx_atemp_nearest.000000' + iter + '.data', xx_atemp_hr)
+write_float64(dirrun_pup + 'xx_theta_hr.000000' + iter + '.data', xx_theta_hr)
+write_float64(dirrun_pup + 'xx_atemp_hr.000000' + iter + '.data', xx_atemp_hr)
 
